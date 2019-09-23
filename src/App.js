dc62c2dac365593e57ecf95486bec201bf80e5c6
anamontiaga <amontiaga@gmail.com>
2019-09-23 15:55:22 +0200

    api request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
+import { fetchCharacters } from "./services/fetchCharacters";
 
-function App() {
-  return <div className="App">:)</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      characters: []
+    };
+  }
+  componentDidMount() {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    fetchCharacters().then(data => {
+      this.setState({ characters: data.results });
+    });
+  }
+
+  render() {
+    const { characters } = this.state;
+    console.log(characters);
+    return <div className="App">:)</div>;
+  }
 }
 
 export default App;
---
+++ src/services/fetchCharacters.js
@@ -0,0 +1,7 @@
+const endpoint = "https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json";
+
+const fetchCharacters = () => {
+  return fetch(endpoint).then(response => response.json());
+};
+
+export { fetchCharacters };
