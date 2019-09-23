6971ba6d641176438ada700dd1189fbf63f079cb
anamontiaga <amontiaga@gmail.com>
2019-09-23 16:15:10 +0200

    paint characters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { fetchCharacters } from "./services/fetchCharacters";
+import logo from "./images/logo.png";
 
 class App extends React.Component {
   constructor(props) {
@@ -23,7 +24,28 @@ class App extends React.Component {
   render() {
     const { characters } = this.state;
     console.log(characters);
-    return <div className="App">:)</div>;
+    return (
+      <div className="App">
+        <div className="app_logo">
+          <img src={logo} alt="Welcome to Rick and Morty world" className="app_logo--img" />
+        </div>
+        <ul className="characters">
+          {characters.map(character => {
+            return (
+              <li className="character" key={`key${character.id}`}>
+                <div className="character__img">
+                  <img src={character.image} alt={`We introduce you to ${character.name}`} />
+                </div>
+                <div className="character__card">
+                  <h3 className="character__card--name">{character.name}</h3>
+                  <h5 className="character__card--species">{character.species}</h5>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
